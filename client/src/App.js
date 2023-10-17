47c13143b4e82aa5bc77df96faa407fc6cd30051
unknown <neetukushwaha8087@gmail.com>
2023-10-17 12:31:10 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+
+function App() {
+  const { user } = useContext(AuthContext);
+  return (
+    <Router>
+      <Routes>
+        <Route exact path='/' element={user ? <Home /> : <Register />}></Route>
+        <Route exact path='/login' element={user ? <Navigate to="/" /> : <Login />}></Route>
+        <Route exact path='/register' element={user ? <Navigate to="/" /> : <Register />}></Route>
+        <Route exact path='/profile/:username' element={<Profile />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
